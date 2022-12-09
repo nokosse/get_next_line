ccfaf42dbd5ce31a6ee18ff063991898cee97898
nokosse <visouthivong.kevin@gmail.com>
2022-12-09 19:26:31 +0100

    added header file

---
+++ get_next_line.h
@@ -0,0 +1,15 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stddef.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <string.h>
+# include <stdint.h>
+
+char	*get_next_line(int fd);
+
+#endif
