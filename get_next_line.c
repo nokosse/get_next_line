0e6620be65fa8ae90a7790b4bc9a6612f6ff59ff
nokosse <visouthivong.kevin@gmail.com>
2022-12-16 13:17:48 +0100

    no segfault, but KO on advanced tests

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 19:26:38 by kvisouth          #+#    #+#             */
-/*   Updated: 2022/12/15 16:32:59 by kvisouth         ###   ########.fr       */
+/*   Updated: 2022/12/16 13:16:16 by kvisouth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,92 +32,83 @@ static int	check_line(char *str)
 }
 
 //Fonction qui supprime tout ce qui est avant le \n dans la string en parametre.
-static char	*ft_strdel(char *str)
+static char	*ft_strcut(char *str)
 {
-	char	*new;
+	char	*cutted_str;
 	int		i;
-
+	int		j;
+	
 	i = 0;
-	while (str[i] != '\n' && str[i])
+	j = 0;
+	while (str[i] != '\n')
 		i++;
+	cutted_str = malloc(sizeof(char) * (ft_strlen(str) - i + 1));
+	if (!cutted_str)
+		return (NULL);
 	i++;
-	new = ft_strdup(str + i);
+	while (str[i])
+		cutted_str[j++] = str[i++];
+	cutted_str[j] = '\0';
 	free(str);
-	return (new);
+	return (cutted_str);
 }
 
 char	*get_next_line(int fd)
 {
-	static char	*stash;	// Variable resultant de la concatenation de buffer et lui meme.
-	char		*buf;	// Variable qui contient ce que read a lu dans son buffer.
-	char		*line;	// Variable qui contient la ligne a retourner.
-	ssize_t		bytes;	// Variable qui contient le nombre de bytes lus par read.
+	static char *stash;	//Variable statique contenant le resultat de la concatenation de lui meme et du buffer.
+	char		*buff;	//Variable contenant le buffer de lecture. (read)
+	char		*line;	//Variable contenant la ligne a retourner.
+	int			readed;	//Variable contenant le nombre de caracteres lus par read.
 
-	if (fd < 0 || BUFFER_SIZE <= 0)	//Retourne NULL si fd OU BUFFER_SIZE est invalide.
+	//On lis une fois avant la boucle pour initialiser les variables.
+	buff = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buff)
 		return (NULL);
 		
-	// Tant qu'on a pas trouver de \n dans stash et qu'on a pas atteint la fin du fichier.
-	// Au cas ou on a qu'une ligne dans le fichier.
-	bytes = 1;
-	while (check_line(stash) == -1)
-	{
-		buf = malloc(sizeof(char) * BUFFER_SIZE + 1);	//// On alloue de la memoire pour buf.
-		if (!buf)										  // 
-			return (NULL);								  // On retourne NULL si malloc a echoue.
-		bytes = read(fd, buf, BUFFER_SIZE);				  // On lit BUFFER_SIZE bytes dans le fichier.
-		if (bytes == 0)
-		{
-			if (stash && ft_strlen(stash) == 1)	 		  // Si on a atteint la fin du fichier et qu'il n'y a pas de \n dans stash.
-			{
-    			line = ft_strdup(stash);				  // On copie stash dans line.
-				free(stash);
-				free(buf);								  // On libere la memoire allouee a buf.
-				return (line);							  // On retourne line.
-			}
-			free(buf);									  // On libere la memoire allouee a buf.
-			break;
-		}
-		if (bytes == 0)									  //
-		{
-			free(buf);									  // On libere la memoire allouee a buf.
-			break;										  // On sort de la boucle si on a atteint la fin du fichier.
-		}
-		if (bytes == -1)								  // 
-		{
-			free(buf);									  // On libere la memoire allouee a buf.
-			return (NULL);								  // On retourne NULL si read a echoue.
-		}
-		buf[bytes] = '\0';								  // On met un \0 a la fin de buf.
-		stash = ft_strjoin(stash, buf);					  // On concatene buf et stash.
-		free(buf);										////
-	}
-
-	if(!stash || stash[1] == '\0')	// Si on a atteint la fin du fichier et qu'il n'y a pas de \n dans stash.
+	readed = read(fd, buff, BUFFER_SIZE); //Juste pour initialiser readed.
+	
+	//On check si read a lu quelque chose, retourne null si il n'y a rien a lire.
+	if (readed <= 0 && !stash)
 		return (NULL);
 
-	// CAS 1 : On a atteint la fin du fichier. Sans aucun \n dans stash. (On a qu'une ligne dans le fichier.)
-	if (bytes == 0 && check_line(stash) == -1)	// Si on a atteint la fin du fichier et qu'il n'y a pas de \n dans stash.
+	buff[readed] = '\0'; //On ajoute le \0 a la fin du buffer.
+	stash = ft_strjoin(stash, buff); //On concatene le buffer a la variable statique.
+	free(buff); //Plus besoin de buff pour le reste de la boucle.
+
+	//On rentre dans le cas ou on a forcement lu quelque chose lors de l'appel a get_next_line.
+	while (readed > 0 || stash) 	//Tant qu'on a pas atteint la fin du fichier.
 	{
-		line = ft_strdup(stash);	// On copie stash dans line.
-		stash = ft_strdel(stash);	// On supprime tout ce qui est avant le \n dans stash.
-		free(stash);				// On libere la memoire de stash.
-		return (line);				// On retourne line.
+		buff = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+		readed = read(fd, buff, BUFFER_SIZE); //On lit le fichier.
+		buff[readed] = '\0'; //On ajoute le \0 a la fin du buffer.
+		stash = ft_strjoin(stash, buff); //On concatene le buffer a la variable statique.
+		free(buff); //Plus besoin de buff pour le reste de la boucle.
+		
+		//CAS 1 : Derniere ligne, pas de \n, read a lu moins que BUFFER_SIZE.
+		if (readed < BUFFER_SIZE && check_line(stash) == -1)
+		{
+			line = stash;
+			stash = NULL;
+			return (line);
+		}
+		//CAS 2 : Pas derniere ligne, il y a un \n dans stash. Read lit forcement BUFFER_SIZE.
+		else if (check_line(stash) != -1)
+		{
+			line = ft_substr(stash, 0, ft_strchr(stash, '\n') - stash + 1);
+			stash = ft_strcut(stash);
+			return (line);
+		}
 	}
-	
-	// CAS 2 : Il y a un \n dans stash. Mais peut etre des trucs apres le \n.
-	line = ft_substr(stash, 0, ft_strchr(stash, '\n') - stash + 1);	// On copie stash dans line jusqu'au \n.
-	stash = ft_strdel(stash);									// On supprime tout ce qui est avant le \n dans stash.
-	return (line);												// On retourne line.
+	//Retourne null si on sort de la boucle (donc qu'on a forcement readed <= 0)
+	return (NULL);
 }
 
 int main(int argc, char **argv)
 {
-	int		fd;
-	int		i;
 	char	*line;
 
-	i = 0;
-	fd = open("text.txt", O_RDONLY);
+	int i = 0;
+	int fd = open("text.txt", O_RDONLY);
 	if (argc == 2)
 	{
 		while (i < atoi(argv[1]))
