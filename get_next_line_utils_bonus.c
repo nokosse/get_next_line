399a6774de6ae9f1f112336371659d32f7f5bacc
nokosse <visouthivong.kevin@gmail.com>
2022-12-22 12:59:52 +0100

    added bonuses

---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,134 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/20 14:05:52 by kvisouth          #+#    #+#             */
+/*   Updated: 2022/12/22 12:59:26 by kvisouth         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+/*
+	It's the exacy same function as ft_strlen from libft.
+*/
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+/*
+	ft_strjoin that works exactly like libft's ft_strjoin.
+	The only difference is that it frees s1.
+	It concatenates s2 to s1 in a new string (str).
+	It is used in GNL to concatenate the stash and the buffer.
+*/
+
+char	*ft_strjoin(const char *s1, const char *s2)
+{
+	char	*str;
+	size_t	size;
+	int		i;
+	int		j;
+
+	size = ft_strlen(s1) + ft_strlen(s2);
+	str = malloc(sizeof(char) * (size + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s1[i])
+		str[j++] = s1[i++];
+	i = 0;
+	while (s2[i])
+		str[j++] = s2[i++];
+	str[j] = '\0';
+	free((char *)s1);
+	return (str);
+}
+
+/*
+	ft_strchr is used to find the \n and is used only
+	as a parameter for ft_substr. (it's size_t len)
+*/
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s != '\0')
+	{
+		if ((unsigned char)*s == (unsigned char)c)
+			return ((char *)s);
+		s++;
+	}
+	if (c == 0)
+		return ((char *)s);
+	return (0);
+}
+
+/*
+	ft_substr is used to get the line from the stash in GNL.
+	In case 1 : Last line, it is doing a strdup of the stash.
+	In case 2 : Not last line, it is strduping from start to \n.
+*/
+
+char	*ft_substr(const char *s, unsigned int start, size_t len)
+{
+	char	*str;
+	int		i;
+	int		y;
+
+	if (start >= ft_strlen(s))
+		start = ft_strlen(s);
+	y = 0;
+	while (start + y < start + len && s[start + y] != '\0')
+		y++;
+	i = 0;
+	str = malloc((y) + 1);
+	if (!str || start > ft_strlen(s))
+		return (NULL);
+	while (i < y)
+	{
+		str[i] = s[start];
+		i++;
+		start++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+/*
+	ft_strcut is used in one case in GNL.
+	When the stash contains a \n, we are cutting the string
+	from the \n to the end of the string, so our new stash does not
+	longer contain the \n ans the past line.
+*/
+
+char	*ft_gnl_strcut(char *str)
+{
+	char	*cutted_str;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	while (str[i] != '\n')
+		i++;
+	cutted_str = malloc(sizeof(char) * (ft_strlen(str) - i + 1));
+	if (!cutted_str)
+		return (NULL);
+	i++;
+	while (str[i])
+		cutted_str[j++] = str[i++];
+	cutted_str[j] = '\0';
+	free(str);
+	return (cutted_str);
+}
